b5d0fa241de7e14e746df4db40ee8aae2825df4d
Jeyalakshmi980 <68963490+Jeyalakshmi980@users.noreply.github.com>
2020-09-23 23:11:03 +0530

    Add files via upload

---
+++ debit_excpetion.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Exception_handling;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Admin
+ */
+public class debit_excpetion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        debit_transact obj=new debit_transact();
+        
+        try
+        {
+            obj.balance_checker();
+        }
+        catch(debitexception d)
+        {
+            System.out.println(d);
+        }
+        
+        
+    }
+    
+}
+class debit_transact
+{
+    Scanner val =new Scanner(System.in);
+    double bankbalance=100000,debitamount;
+    
+    debit_transact()
+    {
+        System.out.println("Available bank balance:Rs."+bankbalance);
+        System.out.println("Enter the amount to be debited:");
+        debitamount=val.nextDouble();
+    }
+    
+    void balance_checker() throws debitexception
+    {       
+        if(debitamount>bankbalance)
+            throw new debitexception("Amount exceeds balance :-( !!");
+        System.out.println("Amount debited successfully :-)  !!\nDebited amount:Rs."+debitamount+"/-\nAvailable balance after debit:Rs."+(bankbalance-debitamount));
+         
+    }
+}
+
+class debitexception extends Exception
+{
+    debitexception(String msg)
+    {
+        super(msg);
+    }
+}
